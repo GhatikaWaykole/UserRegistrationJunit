389ef279170b2c8031394b3de30863fc677f9acc
ghatikawaykole <ghatika99@gmail.com>
2021-10-03 21:21:46 +0530

    welcome to junit program

---
+++ src/main/java/com/example/userregistration/UserRegistration.java
@@ -0,0 +1,10 @@
+package com.example.userregistration;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+    public static void main(String args[]){
+        System.out.println("wlecome to the user gresistration program with junit");
+    }
+}
---
+++ src/test/java/UserRegistrationTest.java
@@ -0,0 +1,9 @@
+import com.example.userregistration.UserRegistration;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapters;
+
+public class UserRegistrationTest {
+    UserRegistration userRegistration = new UserRegistration();
+}

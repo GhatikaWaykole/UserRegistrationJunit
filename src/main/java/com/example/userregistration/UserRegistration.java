205be9be0a9d042cbb5165ec98a000abd2339c77
ghatikawaykole <ghatika99@gmail.com>
2021-10-03 21:37:59 +0530

    Added firstname method to validate the name data

--- src/main/java/com/example/userregistration/UserRegistration.java
+++ src/main/java/com/example/userregistration/UserRegistration.java
@@ -1,10 +1,21 @@
 package com.example.userregistration;
-
+/* purpose write a junit program to validation of user registration program
+ * Validatin use the regex program
+ */
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    public static void main(String args[]){
-        System.out.println("wlecome to the user gresistration program with junit");
+    Pattern pattern;
+    Matcher matcher;
+    /* method firstName to validate the first name of the user
+     * @param name return boolean value
+     */
+    public boolean firstName(String name){
+        String regex = "^[A-Z]{1}[a-z]{2,}";
+        pattern = Pattern.compile(regex);
+        matcher = pattern.matcher(name);
+        return matcher.matches();
     }
 }
--- src/test/java/UserRegistrationTest.java
+++ src/test/java/UserRegistrationTest.java
@@ -6,4 +6,9 @@ import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapters;
 
 public class UserRegistrationTest {
     UserRegistration userRegistration = new UserRegistration();
+    @Test
+    public void givenName_WhenProper_ShouldReturnTrue(){
+       boolean result = userRegistration.firstName("Abcd");
+       Assert.assertTrue(result);
+    }
 }
